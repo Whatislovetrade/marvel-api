b3971de3254f0d005586bc2f1769fa8d3266de77
Vlad Ivannikov <vladmaslow1@gmail.com>
2025-01-12 20:41:20 +0500

    add pagination lazy load

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -4,6 +4,7 @@ class MarvelService {
 
     _apiBase = 'https://gateway.marvel.com:443/v1/public/'
     _apiKey = 'apikey=9af7c2d2be2a1e33321a19377ccad3b1'
+    _baseOffset = 210
 
 
 
@@ -17,8 +18,8 @@ class MarvelService {
        return await res.json()
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`)
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`)
         return res.data.results.map(this._transformCharacter)
     }
 
